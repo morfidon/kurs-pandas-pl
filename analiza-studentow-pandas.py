1938489c42e6ad22eadf1771ac84c9dc433e234a
morfidon <armiksos@gmail.com>
2024-09-30 21:01:32 +0200

    zapis danych

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -4,5 +4,7 @@ df = pd.read_csv("students.csv") # Data Frame
 
 # print(df["Grade"] > 90) # Series
 
-print(df[df["Grade"] > 90])
+high_grade_students = df[df["Grade"] > 90]
+
+high_grade_students.to_csv("studenci_wysokie_oceny.csv", index=False)
 
