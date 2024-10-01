48272f944ac86064500d0b6d301f562e648b6671
morfidon <armiksos@gmail.com>
2024-10-01 12:42:06 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -2,44 +2,11 @@ import pandas as pd
 
 df = pd.read_csv("students.csv") # Data Frame 
 
-#mask 
+
 grade_mask = df["Grade"] > 80
-subject_mask = df["Subject"] == "Math"
-# or and  | &
-# True False
-# 
+# subject_mask = (df["Subject"] == "Math") | (df["Subject"] == "Physics")
 
-print(grade_mask)
-print(subject_mask)
-'''
-     True
-    False
-     True
-     True
-     True
+subject_mask = df["Subject"].isin(["Math", "Physics"])
 
-     True
-    False
-     True
-    False
-    False
 
-    & - koniunkcja - tylko wtedy, gdy OBA WARUNKI są prawdziwe
-    True
-    False
-    True
-    False
-    False
-
-    | - or - lub - alternatywa jest fałszywa, TYLKO wtedy GDY OBA warunki są fałszywe
-    True
-    False
-    True
-    True
-    True
-
-    xor - ^
-    ~ 
-
-'''
-print(df[grade_mask & subject_mask])
+print(df[subject_mask])
