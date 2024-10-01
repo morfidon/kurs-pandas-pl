55da3a1104dbdfdbdefaa6f90d310c5bb89906d3
morfidon <armiksos@gmail.com>
2024-10-01 13:03:22 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -4,9 +4,7 @@ df = pd.read_csv("students.csv") # Data Frame
 
 
 grade_mask = df["Grade"] > 80
-# subject_mask = (df["Subject"] == "Math") | (df["Subject"] == "Physics")
-
 subject_mask = df["Subject"].isin(["Math", "Physics"])
 
-
-print(df[subject_mask])
+# print(df[["Name", "Age"]][subject_mask][grade_mask])
+print(df.loc[subject_mask & grade_mask, ["Name", "Age"]])
