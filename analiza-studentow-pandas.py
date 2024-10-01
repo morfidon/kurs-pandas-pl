06d6b29e9a267f40079046466e55d8116ddfdde0
morfidon <armiksos@gmail.com>
2024-10-01 13:35:08 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -1,7 +1,15 @@
 import pandas as pd
+retrieved_data = {
+    'Name': ['Tom', 'Mia', 'Liam', 'Sophia', 'Jake', 'Olivia'],
+    'Age': [20, 23, 22, 21, 24, 20],
+    'Subject': ['Math', 'Physics', 'Math', 'Chemistry', 'Physics', 'Math'],
+    'Grade': [75, 82, 68, 90, 88, 65]
+} #Dane z innego miejsca
 
 df = pd.read_csv("students.csv") # Data Frame 
 
-df['Missing Points'] = 100 - df['Grade']
-print(df)
+result = pd.concat([df, pd.DataFrame(retrieved_data)], ignore_index=True)
+
+
+print(result)
 
