ed42893bcc1b9d4bc4994d78638d1ceff38d36fc
morfidon <armiksos@gmail.com>
2024-10-01 11:52:51 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -2,9 +2,5 @@ import pandas as pd
 
 df = pd.read_csv("students.csv") # Data Frame 
 
-# print(df["Grade"] > 90) # Series
 
-high_grade_students = df[df["Grade"] > 90]
-
-high_grade_students.to_csv("studenci_wysokie_oceny.csv", index=False)
-
+print(df[["Name", "Age", "Grade"]])
