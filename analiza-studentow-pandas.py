a77fa73c6e62095d34273acb4fbd741b0917a094
morfidon <armiksos@gmail.com>
2024-10-01 18:03:18 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 retrieved_data = {
     'Name': ['Tom', 'Mia', 'Liam', 'Sophia', 'Jake', 'Olivia'],
     'Age': [20, 23, 22, 21, 24, 20],
@@ -8,22 +10,12 @@ retrieved_data = {
 
 df = pd.read_csv("students.csv") # Data Frame 
 
-result = pd.concat([df, pd.DataFrame(retrieved_data)], ignore_index=True)
+df = pd.concat([df, pd.DataFrame(retrieved_data)], ignore_index=True)
 
 
-# result = result.groupby("Subject")["Grade"].mean().reset_index(name="Average Grade")
-# result = result.groupby("Subject")["Age"].max().reset_index(name="Max Age")
-# result = result.groupby("Subject").size().reset_index(name="Amount of students")
+# loc (etykietach) oraz iloc (integer)
 
-result = result.groupby("Subject").agg(
-    AverageGrade=("Grade", "mean"),
-    MaxAge=("Age", "max"),
-    AmountOfStudents=("Name", "count")
-).reset_index()
+# print(100 - df.sort_values(by="Grade", ascending=False).iloc[5:10, 1])
+# print(100 - df.sort_values(by="Grade", ascending=False).iloc[:5, 1])
+print(df.sort_values(by="Grade", ascending=False).loc[:5, "Age"])
 
-# result = result.rename(columns={
-#     "MaxAge": "Maksymalny Wiek",
-#     "Subject": "Przedmiot"
-# })
-result.columns = ["Subject", "Average Grade", "Max Age", "Amount Of Students"]
-print(result)
