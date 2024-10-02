492b0f5a6a6174e7494c0f0dd198be5fbc3b6e35
morfidon <armiksos@gmail.com>
2024-10-02 13:45:02 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -4,15 +4,23 @@ df = pd.read_csv("electronics_store_sales.csv")
 
 df["product"] = df["product"].fillna("Unknown")
 
-product_counts = df["product"].value_counts(dropna=False).astype(int)
-quantity_sold = df.groupby("product")["quantity"].sum().astype(int)
-print(product_counts)
-print(quantity_sold)
+# product_counts = df["product"].value_counts(dropna=False).astype(int)
+# quantity_sold = df.groupby("product")["quantity"].sum().astype(int)
+# print(product_counts)
+# print(quantity_sold)
 
-comparison_df = pd.DataFrame(
-    {
-        "Transaction Count": product_counts,
-        "Total Quantity": quantity_sold
-    }
-)
-print(comparison_df)
+# comparison_df = pd.DataFrame(
+#     {
+#         "Transaction Count": product_counts,
+#         "Total Quantity": quantity_sold
+#     }
+# )
+# print(comparison_df)
+
+# def fill_with_median(series):
+#     return series.fillna(series.median())
+
+print(df["price"].median())
+df['price'] = df.groupby('product')['price'].transform(lambda series: series.fillna(series.median()))
+print(df["price"].median())
+
