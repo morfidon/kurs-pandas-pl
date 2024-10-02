df03d571b0be17170694f7dc5e1f82a69382cfcd
morfidon <armiksos@gmail.com>
2024-10-02 14:38:04 +0200

    wykresy

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -1,12 +1,23 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 df = pd.read_csv("electronics_store_sales.csv")
 
 df["product"] = df["product"].fillna("Unknown")
 
 df['price'] = df.groupby('product')['price'].transform(lambda series: series.fillna(series.median()))
+#
+#
 
+avg_price_by_category = df.groupby('category')['price'].mean().sort_values(ascending=False)
+print(avg_price_by_category)
 
-# -1 do 1
-correlation = df['price'].corr(df['quantity'])
-print(f"\n8. Correlation between price and quantity: {correlation:.2f}")
+plt.figure(figsize=(16, 9))
+sns.barplot(x="category", y="price", data=df)
+plt.plot(avg_price_by_category, 'o-', color='red')
+plt.title("Average Price by Category")
+plt.xlabel('Category')
+plt.ylabel('Average Price')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig('avg_price_by_category.png')
