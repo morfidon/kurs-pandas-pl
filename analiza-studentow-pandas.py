d6801910b4d46bc3ea6e4b2006f28d763b9ba622
morfidon <armiksos@gmail.com>
2024-10-02 16:47:06 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -9,15 +9,7 @@ df['price'] = df.groupby('product')['price'].transform(lambda series: series.fil
 #
 #
 
-avg_price_by_category = df.groupby('category')['price'].mean().sort_values(ascending=False)
-print(avg_price_by_category)
 
-plt.figure(figsize=(16, 9))
-avg_price_by_category.plot(kind='bar', color='red')
-plt.title("Average Price by Category")
-plt.xlabel("Category")
-plt.ylabel("Average Price")
-plt.xticks(rotation=0)
-plt.tight_layout()
-plt.savefig('avg_price_by_category.png')
-
+# Dzienna częstotliwość sprzedaży
+df['date'] = pd.to_datetime(df['date'])
+print(df["date"].value_counts().sort_index())
