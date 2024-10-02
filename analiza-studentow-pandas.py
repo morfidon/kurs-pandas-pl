d2de626a4e0abb23eba5fd2228b43e7bcf8d7c66
morfidon <armiksos@gmail.com>
2024-10-02 15:10:29 +0200

    matplotlib

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 df = pd.read_csv("electronics_store_sales.csv")
 
 df["product"] = df["product"].fillna("Unknown")
@@ -13,11 +13,11 @@ avg_price_by_category = df.groupby('category')['price'].mean().sort_values(ascen
 print(avg_price_by_category)
 
 plt.figure(figsize=(16, 9))
-sns.barplot(x="category", y="price", data=df)
-plt.plot(avg_price_by_category, 'o-', color='red')
+avg_price_by_category.plot(kind='bar', color='red')
 plt.title("Average Price by Category")
-plt.xlabel('Category')
-plt.ylabel('Average Price')
-plt.xticks(rotation=45)
+plt.xlabel("Category")
+plt.ylabel("Average Price")
+plt.xticks(rotation=0)
 plt.tight_layout()
 plt.savefig('avg_price_by_category.png')
+
