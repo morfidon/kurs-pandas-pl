4f3ea94d4e19df78d73617ba21c1c466a57be0b2
morfidon <armiksos@gmail.com>
2024-10-02 12:47:23 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -2,10 +2,9 @@ import pandas as pd
 
 df = pd.read_csv("electronics_store_sales.csv")
 
-# missing_percentage = (df.isnull().sum() / len(df)) * 100
 
-# # print(missing_percentage.round(2))
-# missing_df = missing_percentage.round(2).to_frame(name='Missing data (%)')
-# print(missing_df)
 
-print(df["product"].value_counts(dropna=False))
+product_counts = df["product"].value_counts(dropna=False)
+quantity_sold = df.groupby("product")["quantity"].sum()
+
+print(quantity_sold)
