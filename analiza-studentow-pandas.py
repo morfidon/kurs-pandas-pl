00c4d01c1aa2465f2f04fddff3c146a30fe2f856
morfidon <armiksos@gmail.com>
2024-10-02 13:01:56 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -6,5 +6,13 @@ df["product"] = df["product"].fillna("Unknown")
 
 product_counts = df["product"].value_counts(dropna=False)
 quantity_sold = df.groupby("product")["quantity"].sum()
-
+print(product_counts)
 print(quantity_sold)
+
+comparison_df = pd.DataFrame(
+    {
+        "Transaction Count": product_counts,
+        "Total Quantity": quantity_sold
+    }
+)
+print(comparison_df)
