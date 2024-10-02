0e857a38d217d86a5a4c97e6adf5c64b409f37b7
morfidon <armiksos@gmail.com>
2024-10-02 12:55:23 +0200

    Update analiza-studentow-pandas.py

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv("electronics_store_sales.csv")
 
-
+df["product"] = df["product"].fillna("Unknown")
 
 product_counts = df["product"].value_counts(dropna=False)
 quantity_sold = df.groupby("product")["quantity"].sum()
