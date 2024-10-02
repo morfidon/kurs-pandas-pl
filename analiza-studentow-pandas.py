cfe86972dbd5dd64f521d03e7d37279f296f5be3
morfidon <armiksos@gmail.com>
2024-10-02 11:54:38 +0200

    null

--- analiza-studentow-pandas.py
+++ analiza-studentow-pandas.py
@@ -1,19 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-retrieved_data = {
-    'Name': ['Tom', 'Mia', 'Liam', 'Sophia', 'Jake', 'Olivia'],
-    'Age': [20, 23, 22, 21, 24, 20],
-    'Subject': ['Math', 'Physics', 'Math', 'Chemistry', 'Physics', 'Math'],
-    'Grade': [75, 82, 68, 90, 88, 65]
-} #Dane z innego miejsca
+df = pd.read_csv("electronics_store_sales.csv")
 
-df = pd.read_csv("students.csv") # Data Frame 
-
-df = pd.concat([df, pd.DataFrame(retrieved_data)], ignore_index=True)
-
-
-# loc (etykietach) oraz iloc (integer) - to do wyborow wielu elementow
-# at                    iat (integer) - do pojedynczych wyborow - bo szybsze
-print(df)
-print(df.iat[0, 0])
+print(df.isnull().sum()) #null
